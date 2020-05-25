0c1c0ab398348f35bccf11a9fa35792ab7a16884
Luo Tianj <lt@basecity.com>
2020-05-25 11:39:26 +0800

    basic structure of pull

--- src/flybot/pullable/core.clj
+++ src/flybot/pullable/core.clj
@@ -1,1 +1,51 @@
 (ns flybot.pullable.core)
+
+(defprotocol Query
+  (-select
+   [q data]
+   "select from data"))
+
+(defrecord CoreQuery [key children]
+  Query
+  (-select
+   [_ data]
+   (when key
+     (let [sub-data (get data key)]
+       {key
+        (if-let [children (seq children)]
+          (->> (map #(-select % sub-data) children)
+               (apply merge))
+          sub-data)}))))
+
+(defn query
+  [qspec]
+  (map->CoreQuery qspec))
+
+(defprotocol Queryable
+  (-to-query
+   [queryable]
+   "returns a Query of itself"))
+
+(extend-protocol Queryable
+  nil
+  (-to-query
+    [_]
+    (query {}))
+  Object
+  (-to-query
+    [this]
+    (query {:key this}))
+  clojure.lang.PersistentVector
+  (-to-query
+   [this]
+   (query {:children (map -to-query (.seq this))}))
+  clojure.lang.IPersistentMap
+  (-to-query
+    [this]
+    (if-let [[k v] (first this)]
+      (assoc (-to-query v) :key k)
+      (-to-query nil))))
+
+(defn pattern->query
+  [ptn]
+  (-to-query ptn))
--- test/flybot/pullable/core_test.clj
+++ test/flybot/pullable/core_test.clj
@@ -1,7 +1,28 @@
 (ns flybot.pullable.core-test
   (:require
    [flybot.pullable.core :as sut]
-   [clojure.test :refer [deftest is]]))
+   [clojure.test :refer [deftest testing is]]))
 
-(deftest general
-  (is (= 5 (+ 2 3))))
+(deftest simple
+  (let [data {:foo "bar" :baz {:foo2 3 :baz2 'ok}}]
+    (testing "empty query should always return empty"      
+      (is (= nil (sut/-select (sut/query {}) data))))
+    (testing "query with a single key will just returns a single kv map"
+      (is (= {:foo "bar"} (sut/-select (sut/query {:key :foo}) data))))
+    (testing "when query has children, it will pass sub data to them"
+      (is (= {:baz {:foo2 3}} (sut/-select (sut/query {:key :baz :children [(sut/query {:key :foo2})]}) data)))
+      (is (= {:baz {:foo2 3 :baz2 'ok}}
+             (sut/-select (sut/query {:key :baz :children [(sut/query {:key :foo2})
+                                                           (sut/query {:key :baz2})]}) data))))))
+
+(deftest pattern->query
+  (testing "nil pattern makes an empty query"
+    (is (= (sut/query {}) (sut/pattern->query nil))))
+  (testing "single element pattern makes a simple query"
+    (is (= (sut/query {:key :a}) (sut/pattern->query :a))))
+  (testing "root vector will make a root empty query with children"
+    (is (= (sut/query {:children [(sut/query {:key :a}) (sut/query {:key :b})]})
+           (sut/pattern->query [:a :b]))))
+  (testing "map makes a query with key and children"
+    (is (= (sut/query {:key :a :children [(sut/query {:key :b})]})
+           (sut/pattern->query {:a [:b]})))))
