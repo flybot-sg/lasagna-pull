df9540cb92f030c5fc380d3e850a70d899f6714f
Luo Tianj <lt@basecity.com>
2020-11-19 16:21:09 +0800

    failed test for join query

--- src/robertluo/pullable/core.clj
+++ src/robertluo/pullable/core.clj
@@ -57,9 +57,7 @@
   [k-query v-query target m]
   (let [v (-value-of k-query m)]
     (-append target (-key k-query)
-             (if (= v ::none)
-               ::none
-               (-transform v-query (empty v) v)))))
+             (-transform v-query (empty v) v))))
 
 (defrecord JoinQuery [context k-query v-query]
   Query
