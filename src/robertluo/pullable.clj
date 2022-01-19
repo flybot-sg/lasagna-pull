c6a592978e4338712736978b135529027ff8a6ee
Luo Tianj <lt@basecity.com>
2022-01-19 23:40:05 +0800

    pullable documentation

--- src/robertluo/pullable.clj
+++ src/robertluo/pullable.clj
@@ -40,20 +40,22 @@
    variable `?`, you can get informatioon from the potentially massive data.
 
      - Map pattern: `{...}`, mimic the shape of the map to be queried
+   
      - Filter pattern: If you specified a concrete value for a given key in a map, it works
        like a filter, i.e., pattern `{:a ? :b 1}` will return an empty map on data `{:a 1 :b 2}`.
        If you use same logical variabl in multiple places of a pattern, it has to be the
        same value, so if we can not satisfy this, the matching fails. i.e., 
        pattern `'{:a ?x :b ?x}` returns an empty map on data `{:a 2 :b 3}`.
+   
      - Options: by enclosing a key in map pattern with a list `()`, and optional pairs of
        options, you can process value after a match or miss. i.e., pattern
        `{(:a :not-found ::ok) ?}` on data `{:b 5}` having a matching result of `{:a ::ok}`.
        Various options supported, and can be defined by multimethod of `core/apply-post`.
+   
      - Sequence pattern: For sequence of maps, using `[]` to enclose it. Sequence pattern
        can have an optional variable and options. i.e., pattern 
        `'[{:a ?} ?]` on `[{:a 1} {:a 3} {}]` has a matching result of
-       `[{:a 1} {:a 3} {}]`.
-   "
+       `[{:a 1} {:a 3} {}]`. "
   [pattern]
   (with-meta
     (fn [f-named-var]
