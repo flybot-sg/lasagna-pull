7fb9aa837a07e933b9f6fe3448ffb254e7031f41
loicb <loic.blanchardDev@gmail.com>
2022-07-23 11:39:28 +0800

    Basic client data and general pattern merging (#41)

--- src/sg/flybot/pullable/schema.clj
+++ src/sg/flybot/pullable/schema.clj
@@ -3,8 +3,8 @@
 
 (ns sg.flybot.pullable.schema
   "Pattern validation with Malli schema."
-  (:require
-   [malli.core :as m]))
+  (:require [malli.core :as m]
+            [malli.util :as mu]))
 
 (defn lvar?
   [x]
@@ -24,9 +24,11 @@
                [:k-with [:catn
                          [:k :keyword]
                          [:options [:* ::option]]]]]
+   ::var      [:orn
+               [:var   [:= '?]]
+               [:named [:fn lvar?]]]
    ::val      [:orn
-               [:var    [:= '?]]
-               [:named  [:fn lvar?]]
+               ::var
                [:nested [:ref ::pattern]]
                [:filter :any]]
    ::vector   [:map-of ::key ::val]
@@ -41,17 +43,60 @@
                ::vector
                ::seq]})
 
+(defn update-child
+  "Adds pattern info to the given `child` when applicable.
+   `options` contains the required registries notably.
+   i.e. [:a {:min 1} :int] => [:a {:min 1} [:or ::var :int]]"
+  [child options]
+  (if (vector? child)
+    (let [[k o v] child]
+      (if (or (some #{k} [:vector :sequential :set])
+              (mu/find-first v (fn [sch _ _] (= :map (m/type sch))) options))
+        child
+        [k o [:or ::var v]]))
+    child))
+
+(defn combine-data-pattern
+  "Walks through the `data-schema` and updates it with pattern registry when applicable."
+  [data-schema] 
+  (m/walk
+   data-schema
+   (fn [schema _ children options]
+     (let [children (if (m/children schema)
+                      (map #(update-child % options) children)
+                      children)]
+       (m/into-schema
+        (m/type schema) (m/properties schema) children options)))
+   (m/options data-schema)))
+
+(defn to-vector-syntax
+  "Takes a schema syntax and converts it to the vector syntax such as:
+   [:schema properties children]"
+  [syntax]
+  (let [v-schema (if (or (vector? syntax) (m/schema? syntax))
+                   (m/schema syntax)
+                   (m/from-ast syntax))]
+    (if (= :schema (m/type v-schema))
+      v-schema
+      [:schema nil v-schema])))
+
 (defn pattern-validator
   "returns a function which can validate query pattern.
    - `data-schema`: user provided schema for data"
-  ([data-schema]
-   (m/validator
-    (if data-schema
-      (throw (UnsupportedOperationException. "Not implemented yet"))
-      [:schema
-       {:registry general-pattern-registry}
-       ::pattern]))))
+  [data-schema]
+  (m/validator
+   (if data-schema
+     (let [schema        (-> data-schema to-vector-syntax m/children first)
+           data-registry (-> data-schema m/properties :registry)]
+       (combine-data-pattern
+        [:schema
+         {:registry ((fnil merge {}) data-registry general-pattern-registry)}
+         [:and ::pattern schema]]))
+     [:schema
+      {:registry general-pattern-registry}
+      ::pattern])))
 
 (comment
-  ((pattern-validator nil) '{:a ? :b {:c ?a}})
-  )
+  ((pattern-validator nil) '{:a ?a (:b :with [3]) {:c ?c}})
+  ((pattern-validator [:schema nil [:map [:a :int] [:b [:map [:c :string]]]]])
+   {:a '?a :b {:c '?c}}))
--- test/sg/flybot/pullable/schema_test.clj
+++ test/sg/flybot/pullable/schema_test.clj
@@ -1,54 +1,122 @@
 (ns sg.flybot.pullable.schema-test
-  (:require
-   [sg.flybot.pullable.schema :as sut]
-   [clojure.test :refer [deftest are testing]]))
+  (:require [clojure.test :refer [is are deftest testing]]
+            [malli.core :as m]
+            [malli.util :as mu] 
+            [sg.flybot.pullable.schema :as sut]))
 
+(deftest to-vector-syntax
+  (testing "Normal vector to schema vector."
+    (are [syntax] (mu/equals [:schema nil :int]
+                             (sut/to-vector-syntax syntax))
+      ;; vector syntax 
+      [:schema :int]
+      ;; schema vector syntax
+      (m/schema [:schema :int])
+      ;; schema map syntax
+      {:type :int})))
 
-(deftest pattern-valid?
-  (testing "Pattern is valid so returns it."
+(deftest combine-data-pattern
+  (testing "Combines the data schema with the pattern registry."
+    (are [sch-data sch-exp] (mu/equals
+                             [:schema {:registry sut/general-pattern-registry} sch-exp]
+                             (sut/combine-data-pattern
+                              [:schema {:registry sut/general-pattern-registry} sch-data]))
+      ;; simple map
+      [:map [:a :int] [:b :string]]
+      [:map [:a [:or ::sut/var :int]] [:b [:or ::sut/var :string]]]
+
+      ;; conserve malli option map
+      [:map {:description "ok" :closed true} [:a {:min 1} :int]]
+      [:map {:description "ok" :closed true} [:a {:min 1} [:or ::sut/var :int]]]
+
+      ;; nested map
+      [:map [:a [:map [:b :string]]]]
+      [:map [:a [:map [:b [:or ::sut/var :string]]]]]
+
+      ;; simple seq - not seq of pattern
+      [:map [:a [:sequential :int]]]
+      [:map [:a [:or ::sut/var [:sequential :int]]]]
+
+      ;; nested seq - not seq of pattern
+      [:map [:a [:sequential [:set [:vector :int]]]]]
+      [:map [:a [:or ::sut/var [:sequential [:set [:vector :int]]]]]]
+
+      ;; simple seq
+      [:map [:a [:sequential [:map [:b :int]]]]]
+      [:map [:a [:sequential [:map [:b [:or ::sut/var :int]]]]]]
+
+      ;; nested seq
+      [:vector
+       [:map
+        [:a [:set [:map [:b :keyword]]]]]]
+      [:vector
+       [:map
+        [:a [:set [:map [:b [:or ::sut/var :keyword]]]]]]])))
+
+(deftest pattern-validator
+  (testing "No data provided case: pattern is valid."
     (are [p] (true? ((sut/pattern-validator nil) p))
-    ;;basic patterns
+      ;;basic patterns
       '{:a ?}
       '{:a ? :b ?}
 
-    ;;filtered
+      ;;filtered
       '{:a ? :b 1}
 
-    ;;filter with a function
+      ;;filter with a function
       {:a '? :b even?}
 
-    ;;guard clause
+      ;;guard clause
       {(list :a :when even?) '?}
 
-    ;;guard with not-found
+      ;;guard with not-found
       {(list :a :when even? :not-found 0) '?}
 
-    ;;with option 
+      ;;with option
       '{(:a :with [3]) ?a}
       '{(:a :with [{:b 2 :c 3}]) {:b ?}}
 
-    ;;seq query
+      ;;seq query
       '[{:a ?}]
 
-    ;;nested map query 
+      ;;nested map query
       '{:a {:b ?}}
       '{:a {:b [{:c ?}]}}
 
-    ;;named variable 
+      ;;named variable
       '{:a ?a}
 
-    ;;named variable join
+      ;;named variable join
       '{:a ?x :b {:c ?x}}
       '{:a ?x :b ?x}
 
-    ;;named join 
+      ;;named join
       '{:a ?a :b ?a}
 
-    ;;capture a sequence 
+      ;;capture a sequence
       '[{:a ? :b ?} ?g]
 
-    ;;seq option 
+      ;;seq option
       '[{:a ? :b ?} ? :seq [2 3]]
 
-    ;;batch option 
-      '{(:a :batch [[3] [{:ok 1}]]) ?a})))
+      ;;batch option
+      '{(:a :batch [[3] [{:ok 1}]]) ?a}))
+
+      (testing "data + pattern are valid."
+        (is (true? ((sut/pattern-validator
+                     [:schema 
+                      {:registry {::test :string}}
+                      [:map [:a :int] [:b ::test]]])
+                    {:a '?a :b "ok"}))))
+      (testing "data valid but pattern invalid."
+        (is (false? ((sut/pattern-validator
+                     [:schema
+                      {:registry {::test :string}}
+                      [:map [:a :int] [:b ::test]]])
+                    {:c '?c}))))
+      (testing "pattern valid but data invalid."
+        (is (false? ((sut/pattern-validator
+                      [:map [:a :int] [:b :int]])
+                    {:a '?a :b "ok"}))))
+      ;;TODO: data-schema does not work with options (:with, :seq etc)
+      )
