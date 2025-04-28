b8996924327012792bea38f6f3338f5b03c2e323
Luo Tian <lt@basecity.com>
2025-04-28 17:54:30 +0800

    add pom-data per clojar's request

--- build.clj
+++ build.clj
@@ -3,15 +3,32 @@
   (:require [clojure.tools.build.api :as b]
             [org.corfield.build :as cb]))
 
-(defn project 
+(defn project
   "apply project default to `opts`"
   [opts]
-  (let [defaults {:lib     'sg.flybot/lasagna-pull
-                  :version (format "0.4.%s" (b/git-count-revs nil))
-                  :scm     {:url "https://github.com/flybot-sg/lasagna-pull"}}]
+  (let [version (format "0.4.%s" (b/git-count-revs nil))
+        defaults {:lib     'sg.flybot/lasagna-pull
+                  :version version}]
     (merge defaults opts)))
 
-(defn tests 
+(defn pom
+  [opts]
+  (let [pom-data
+        [[:description "Precisely select from deep data structure"]
+         [:url "https://github.com/flybot-sg/lasagna-pull"]
+         [:licenses
+          [:license
+           [:name "Apache-2.0"]
+           [:url "https://www.apache.org/licenses/LICENSE-2.0.txt"]]]
+         [:organization "Flybot Pte Ltd"]]
+        opts (merge opts
+                    {:basis (b/create-basis {})
+                     :target "target"
+                     :pom-data pom-data})]
+    (b/write-pom opts)
+    opts))
+
+(defn tests
   "run all tests, for clj and cljs."
   [opts]
   (-> opts
@@ -24,6 +41,7 @@
       (project)
       (cb/clean)
       (tests)
+      (pom)
       (cb/jar)))
 
 (defn deploy
